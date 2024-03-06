61a232251eb0cbeb90fef1c09dea66275e848bb2
zciwolvo <zciwolvo@gmail.com>
2024-03-06 12:40:13 +0100

    second script not working yet

--- test.py
+++ test.py
@@ -27,5 +27,16 @@ response_data = json.loads(response.text)
 
 access_token = response_data['access_token']
 
-print(response_data)
+gen_url = INGAPI.INGAPI(host="https://api.sandbox.ing.com", client_id="5ca1ab1e-c0ca-c01a-cafe-154deadbea75", endpoint="/oauth2/authorization-server-url?scope=payment-accounts%3Abalances%3Aview%20payment-accounts%3Atransactions%3Aview&redirect_uri=https://www.example.com&country_code=NL")
+
+response_url = gen_url.query(
+    method=httpMethod,
+    body=payload,
+    token=access_token,
+    endpoint="/oauth2/authorization-server-url?scope=payment-accounts%3Abalances%3Aview%20payment-accounts%3Atransactions%3Aview&redirect_uri=https://www.example.com&country_code=NL"
+)
+
+response_url_data = json.loads(response_url.text)
+
+print(response_url_data)
 
